9f58922606126cef9a58cdd5f31a7d8fe20848c2
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-05 11:56:06 +0200

    Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,22 @@
+/* Realiza un programa que pida dos números y que luego muestre el resultado
+de su multiplicación. */
+import java.util.Scanner;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce el primer numero: ");
+        int n1 = sc.nextInt();
+
+        System.out.print("Introduce el segundo numero: ");
+        int n2 = sc.nextInt();
+
+        int multiplicacion;
+        multiplicacion = n1 * n2;
+
+        System.out.println("El resultado es: " + multiplicacion);
+
+        sc.close();
+
+    }
+}
